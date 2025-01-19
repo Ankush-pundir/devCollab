8a806b67953ab5b5bf0e77f0d11b64efb1955656
Ankush-pundir <pundirankush2@gmail.com>
2025-01-19 19:22:07 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,10 @@
+console.log("hello");
+const express=require("express");
+const app=express();
+app.use('/',(req,res)=>{
+    res.send("hello world");
+})
+app.listen(3000,()=>{
+    
+    console.log("listening");
+});
